b6446f3a5811e6d8a886f69fc196b872804e7ac7
poyrazemun <ofatih-poyraz@hotmail.com>
2022-06-29 17:03:20 +0200

    Initial commit

---
+++ src/test/java/com/intabella/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.intabella.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber-report.html"
+        },
+        features = "src/test/resources/features",
+        glue = "com/intabella/step_definitions",
+        tags = ""
+
+)
+
+public class CukesRunner {
+
+}
